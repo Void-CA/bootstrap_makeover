8623eb9dcf7b22c89c666ed239cf2536364aa2e5
Void-CA <castilloari282@gmail.com>
2026-02-10 10:37:35 -0600

    feat(js): new buttom with basic functionality of removing the table

---
+++ calendar_format.js
@@ -0,0 +1,28 @@
+const calendarBtn = document.getElementById('calendar');
+let contentContainer = document.getElementById('content-container');
+let calendarActive = false;
+
+function toggleBtnText() {
+    if (!calendarActive) {
+        calendarBtn.innerHTML = '<i class="bi bi-table"></i> Volver a formato de Tabla';
+        calendarActive = true;
+    }
+    else {
+        calendarBtn.innerHTML = '<i class="bi bi-calendar"></i> Cambiar a formato de Calendario';
+        calendarActive = false; 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+calendarBtn.addEventListener('click', () => {
+    // Aquí puedes agregar la lógica para cambiar al formato de calendario
+    toggleBtnText();
+    });
